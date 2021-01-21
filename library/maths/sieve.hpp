eb271ac1a9cf90bc0c74e4815ab972630850a7c7
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-01-22 02:12:15 +1000

    Clean up sieve

--- library/maths/sieve.hpp
+++ library/maths/sieve.hpp
@@ -7,24 +7,27 @@ using namespace std;
 /// Sieve of Eratosthenes
 /// Benchmark: 3314 ms/188.74 Mib for N = 5 * 1e8
 /// Credit: KTH's notebook
-constexpr int MAX_N = (int) 5 * 1e8;
-bitset<MAX_N + 1> is_prime;
-vector<int> primes;
 
-void sieve(int N) {
-    is_prime.set();
-    is_prime[0] = is_prime[1] = 0;
+namespace eratosthenes {
+    constexpr int MAX_N = (int) 5 * 1e8;
+    bitset<MAX_N + 1> is_prime;
+    vector<int> primes;
 
-    for (int i = 4; i <= N; i += 2) is_prime[i] = 0;
+    void sieve(int N) {
+        is_prime.set();
+        is_prime[0] = is_prime[1] = 0;
 
-    for (int i = 3; i * i <= N; i += 2) {
-        if (!is_prime[i]) continue;
-        for (int j = i * i; j <= N; j += i * 2) {
-            is_prime[j] = 0;
+        for (int i = 4; i <= N; i += 2) is_prime[i] = 0;
+
+        for (int i = 3; i * i <= N; i += 2) {
+            if (!is_prime[i]) continue;
+            for (int j = i * i; j <= N; j += i * 2) {
+                is_prime[j] = 0;
+            }
         }
-    }
 
-    for (int i = 2; i <= N; i++) {
-        if (is_prime[i]) primes.push_back(i);
+        for (int i = 2; i <= N; i++) {
+            if (is_prime[i]) primes.push_back(i);
+        }
     }
 }
